55b7ce535e33e8808b95b07bf6a1af7d3a138e2f
melinaapricot <melinaverikios@gmail.com>
2021-12-09 18:56:57 +0100

    restructure components

--- src/components/DestinationCard.vue
+++ src/components/DestinationCard.vue
@@ -1,85 +1,55 @@
 <template>
-  <div class="card">
-    <div class="card__overlay"/>
-    <div v-for="destination of destinations" class="card__wrapper" :key="destination.title">
-      <img class="card__img" :src="destination.image" alt="Photograph of the destination">
-      <div class="card__description">
-        <p class="card__description--destination">{{destination.text}}</p>
-        <p class="card__description--price"><span>ab </span>{{destination.price}}</p>
-      </div>
+
+  <div class="card__wrapper" :key="destination.title">
+    <img class="card__img" :src="destination.image" alt="Photograph of the destination">
+    <div class="card__description">
+      <p class="card__description--destination">{{ destination.text }}</p>
+      <p class="card__description--price"><span>ab </span>{{ destination.price }}</p>
     </div>
   </div>
 </template>
 
 <script lang="js">
-  import DataProvider from "../io/DataProvider";
-
-  export default {
-    data() {
-      return {
-        destinations: [],
-        error: "",
-      };
-    },
-    mounted() {
-      const provider = new DataProvider();
-      provider.fetchData()
-        .then(data => this.destinations = data)
-        .catch(() => this.error = "Could not fetch destination data")
-
-    }
-  }
+export default {
+  props: {destination: Object}
+}
 
 </script>
 
 <style lang="css">
+.card__wrapper {
+  display: inline-flex;
+  flex-direction: column;
+  box-shadow: -2px 2px 0 var(--green);
+  border-radius: 15px;
+  overflow: hidden;
+  flex-shrink: 0;
 
-  .card {
-    overflow: auto;
-    display: flex;
-    padding-bottom: 4px;
-    gap: 23px;
-  }
-
-  .card__overlay {
-    position: absolute;
-    right: 0;
-    background: linear-gradient(to left, white, transparent);
-    z-index: 5;
-    height: 225px;
-    width: 22px
-  }
-
-  .card__wrapper {
-    display: inline-flex;
-    flex-direction: column;
-    box-shadow:  -2px 2px 0 var(--green);
-    border-radius: 15px;
-    overflow: hidden;
-    flex-shrink: 0;
-
-  }
+}
 
 
-  .card__img {
-    object-fit: cover;
-    width: 287px;
-    height: 177px;
-  }
-  .card__description {
-    display: flex;
-    justify-content: space-between;
-    padding: 12px;
-  }
+.card__img {
+  object-fit: cover;
+  width: 287px;
+  height: 177px;
+}
 
-  .card__description--destination {
-    color: var(--primary);
-  }
-  .card__description--price {
-    color: var(--purple);
-  }
-  .card__description--price span {
-    font-size: 12px;
-  }
+.card__description {
+  display: flex;
+  justify-content: space-between;
+  padding: 12px;
+}
+
+.card__description--destination {
+  color: var(--primary);
+}
+
+.card__description--price {
+  color: var(--purple);
+}
+
+.card__description--price span {
+  font-size: 12px;
+}
 
 </style>
--- src/components/DestinationListView.vue
+++ src/components/DestinationListView.vue
@@ -1,39 +1,76 @@
 <template>
   <div class="list-view">
-    <div class="list-view__header-upper">
+    <div class="header-wrapper">
       <h2>Länder, Regionen, Orte</h2>
-      <a class="list-view__all-countries" href="#">Alle</a>
+      <a class="all-countries" href="#">Alle</a>
     </div>
     <p>Lorem Ipsum Länder, Regionen, Orte</p>
-    <DestinationCard/>
+
+    <div class="card-list">
+      <div class="card-overlay"/>
+      <DestinationCard v-for="destination of destinations" :key="destination.text" :destination="destination"/>
+    </div>
   </div>
 </template>
 
 <script>
-  import DestinationCard from "./DestinationCard";
-  export default {
-    name: 'DestinationListView',
-    components: {
-      DestinationCard,
-    }
+import DestinationCard from "./DestinationCard";
+import DataProvider from "../io/DataProvider";
+
+export default {
+  name: 'DestinationListView',
+  components: {
+    DestinationCard,
+  },
+  data() {
+    return {
+      destinations: [],
+      error: "",
+    };
+  },
+  mounted() {
+    const provider = new DataProvider();
+    provider.fetchData()
+        .then(data => this.destinations = data)
+        .catch(() => this.error = "Could not fetch destination data")
   }
+}
 </script>
 
-<style>
+<style scoped>
+.list-view {
+  margin-left: 19px;
+}
 
-  .list-view__header-upper{
-    display: inline-flex;
-    justify-content: space-between;
-    align-items: baseline;
+.header-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
 
-  }
 
-  .list-view__all-countries {
-    color: var(--primary);
-  }
+}
 
-  .list-view__all-countries:link {
-    text-decoration: none;
-  }
+.all-countries {
+  color: var(--primary);
+}
 
+.all-countries:link {
+  text-decoration: none;
+}
+
+.card-list {
+  overflow: auto;
+  display: flex;
+  padding-right: 16px;
+  gap: 23px;
+}
+
+.card-overlay {
+  position: absolute;
+  right: 0;
+  background: linear-gradient(to left, white, transparent);
+  z-index: 5;
+  height: 100%;
+  width: 22px
+}
 </style>
